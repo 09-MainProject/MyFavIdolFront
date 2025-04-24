f20cb8ad612c95c3d2305447d1e14ab6c76a2d54
Leekwanyong <dlrkdid12@gmail.com>
2025-04-25 03:02:51 +0900

    🔧 chore: ESLint alias 설정 문제 해결 및 CI 환경 대응

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { createBrowserRouter, RouterProvider } from 'react-router';
-import Layout from '@/components/layouts/Layout';
-import Home from '@/pages/home/Home';
 import Artist from '@/pages/artists/Artist';
-import Schedule from '@/pages/schedule/Schedule';
-import Timeline from '@/pages/timeline/Timeline';
+import Home from '@/pages/home/Home';
 import Login from '@/pages/signup/Login';
+import Schedule from '@/pages/schedule/Schedule';
 import Signup from '@/pages/signup/Signup';
+import Timeline from '@/pages/timeline/Timeline';
+import Layout from '@/components/layouts/Layout';
 
 function App() {
   const router = createBrowserRouter([
