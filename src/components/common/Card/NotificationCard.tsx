7917dec43e85b52a83612aecc77b54c6871bf794
Leekwanyong <dlrkdid12@gmail.com>
2025-04-27 22:13:41 +0900

    :lipstick: style: #14 NotificationCard mt 수정

--- src/components/common/Card/NotificationCard.tsx
+++ src/components/common/Card/NotificationCard.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 function NotificationCard({ children }: Props) {
   return (
-    <div className="mt-12 flex max-h-[300px] cursor-pointer flex-col rounded-xl p-4 shadow-lg">
+    <div className="mt-4 flex max-h-[300px] cursor-pointer flex-col rounded-xl p-4 shadow-lg">
       {children}
     </div>
   );
