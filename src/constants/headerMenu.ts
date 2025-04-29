e881c65bb9c9b7f1dda8305ebf4c619f9a3ce56c
Leekwanyong <dlrkdid12@gmail.com>
2025-04-29 14:02:31 +0900

    :recycle: refactor: HEADER_MENU에 타입 정의 추가 및 구조 개선

--- src/constants/headerMenu.ts
+++ src/constants/headerMenu.ts
@@ -1,4 +1,9 @@
-export const HEADER_MENU = [
+export interface HeaderMenuItem {
+  title: string;
+  href: string;
+}
+
+export const HEADER_MENU: HeaderMenuItem[] = [
   { title: 'Artists', href: '/artists' },
   { title: 'Schedule', href: '/schedule' },
   { title: 'TimeLine', href: '/timeline' },
