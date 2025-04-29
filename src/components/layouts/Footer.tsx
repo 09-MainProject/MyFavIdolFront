a2d179dc78e4950fcf29b7f9b87da6ecfd856611
kjy0124 <0124kimjy@naver.com>
2025-04-29 23:24:14 +0900

    ✨feat: #37 footer UI 구현

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,5 +1,35 @@
+import { businessInfo, customerServiceInfo } from '@/constants/footerInfo';
+
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-black px-6 py-10 text-sm text-gray-400">
+      <div className="mx-auto flex max-w-6xl flex-col justify-between gap-10 md:flex-row">
+        <div>
+          <h4 className="mb-2 text-xl font-bold">{businessInfo.title}</h4>
+          <ul className="space-y-1">
+            {businessInfo.items.map((item, index) => (
+              <li key={`${index * 12345}`}>{item}</li>
+            ))}
+          </ul>
+        </div>
+
+        <div>
+          <h4 className="mr-80 mb-2 text-xl font-bold">
+            {customerServiceInfo.title}
+          </h4>
+          <ul className="space-y-1">
+            {customerServiceInfo.items.map((item, index) => (
+              <li key={`${index * 12345}`}>{item}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+      <div className="mt-10 text-center text-xs text-gray-400">
+        CompanyName © 2025. All rights reserved.
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
