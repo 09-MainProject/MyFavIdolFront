9149de56e48390b59c00d8b9c28c2ce2ac0b31e5
han <pjjh125@gmail.com>
2025-04-30 11:05:18 +0900

    ✨ feat: 회원가입(Sign Up) 기능 구현 및 반응형 UI 작업

---
+++ src/assets/icons/NaverIcon.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function NaverIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 111 111"
+      width="24"
+      height="24"
+    >
+      <rect fill="#06be34" width="111" height="111" rx="9.05" ry="9.05" />
+      <polygon
+        fill="#ffffff"
+        points="83.07 30.16 83.07 80.85 64.57 80.85 46.44 55.33 46.44 80.85 27.94 80.85 27.94 30.16 46.44 30.16 64.57 56.7 64.57 30.16 83.07 30.16"
+      />
+    </svg>
+  );
+}
