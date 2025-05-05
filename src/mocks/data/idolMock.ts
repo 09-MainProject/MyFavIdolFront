17bd9566bcbbcbbf54f77644549745b95a512718
Leekwanyong <dlrkdid12@gmail.com>
2025-05-05 17:19:17 +0900

    :sparkles: feat: #58 아이돌 목데이터 구현

---
+++ src/mocks/data/idolMock.ts
@@ -0,0 +1,91 @@
+export type IdolMock = {
+  idol_id: number;
+  name: string;
+  debut_date: string;
+  agency: string;
+  description: string;
+  profile_image: string;
+};
+
+export const idolsMock: IdolMock[] = [
+  {
+    idol_id: 100,
+    name: '오즈코딩',
+    debut_date: '2024-04-23',
+    agency: '오코ent',
+    description: '코딩 던지는 아이돌',
+    profile_image: 'https://example.com/images/ozcoding.jpg',
+  },
+  {
+    idol_id: 101,
+    name: '디버깅즈',
+    debut_date: '2023-06-15',
+    agency: '디버그뮤직',
+    description: '에러를 무대에서 해결하는 천재돌',
+    profile_image: 'https://example.com/images/debuggers.jpg',
+  },
+  {
+    idol_id: 102,
+    name: '컴파일러즈',
+    debut_date: '2022-11-30',
+    agency: '스택오버플로우ent',
+    description: '빠르게 컴파일하는 댄스돌',
+    profile_image: 'https://example.com/images/compilerz.jpg',
+  },
+  {
+    idol_id: 103,
+    name: '리액트걸즈',
+    debut_date: '2021-08-01',
+    agency: 'JSent',
+    description: '반응형 무대를 펼치는 프론트돌',
+    profile_image: 'https://example.com/images/reactgirls.jpg',
+  },
+  {
+    idol_id: 104,
+    name: '넥스트제너',
+    debut_date: '2020-02-12',
+    agency: '넥제엔터테인먼트',
+    description: '차세대 K-pop을 이끄는 실험돌',
+    profile_image: 'https://example.com/images/nextgen.jpg',
+  },
+  {
+    idol_id: 105,
+    name: '타입스크립트즈',
+    debut_date: '2023-03-27',
+    agency: 'TS Agency',
+    description: '정적 타입처럼 확실한 매력을 가진 그룹',
+    profile_image: 'https://example.com/images/typescriptz.jpg',
+  },
+  {
+    idol_id: 106,
+    name: '서버사이드보이즈',
+    debut_date: '2019-09-09',
+    agency: '백엔드뮤직',
+    description: '서버처럼 묵직한 보컬',
+    profile_image: 'https://example.com/images/ssboys.jpg',
+  },
+  {
+    idol_id: 107,
+    name: 'GITLUV',
+    debut_date: '2022-12-14',
+    agency: '버전컨트롤ent',
+    description: '변화를 커밋하는 열정돌',
+    profile_image: 'https://example.com/images/gitluv.jpg',
+  },
+  {
+    idol_id: 108,
+    name: '쿠키즈',
+    debut_date: '2021-07-17',
+    agency: '세션엔터',
+    description: '기억에 남는 맛을 가진 무대',
+    profile_image: 'https://example.com/images/cookies.jpg',
+  },
+  {
+    idol_id: 109,
+    name: '프록시',
+    debut_date: '2024-01-05',
+    agency: '리버스엔터테인먼트',
+    description: '항상 너와 연결되는 프록시돌',
+    profile_image: 'https://example.com/images/proxy.jpg',
+  },
+];
