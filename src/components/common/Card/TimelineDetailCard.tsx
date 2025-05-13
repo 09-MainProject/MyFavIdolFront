fb707ce3c1704e6be6bdaca9f448f46dcefc9ecf
Leekwanyong <dlrkdid12@gmail.com>
2025-05-13 15:50:08 +0900

    :recycle: refactor: #68 TimelineDetailCard.tsx ?옵셔널을 이용해 안전성 추가

--- src/components/common/Card/TimelineDetailCard.tsx
+++ src/components/common/Card/TimelineDetailCard.tsx
@@ -1,77 +1,77 @@
 import React from 'react';
-import { Link } from 'react-router';
-import { Comment, Frame, Heart } from '@assets/icons/inedx';
+import {Link} from 'react-router';
+import {Comment, Frame, Heart} from '@assets/icons/inedx';
 import Dropdown from '@components/common/Dropdown.tsx';
 import useDropdownToggle from '@hooks/useDropdownToggle.tsx';
-import { Idol } from '@store/idolStore';
+import {Idol} from '@store/idolStore';
 
 type Props = {
-  idol: Idol;
-  handleDeletePost: () => void;
+    idol: Idol;
+    handleDeletePost: () => void;
 };
 
-function TimelineDetailCard({ idol, handleDeletePost }: Props) {
-  const { ref, isIdolDropdownOpen, handleToggleIdolDropdown } =
-    useDropdownToggle();
+function TimelineDetailCard({idol, handleDeletePost}: Props) {
+    const {ref, isIdolDropdownOpen, handleToggleIdolDropdown} =
+        useDropdownToggle();
 
-  return (
-    <div className="mt-8 rounded-xl bg-white">
-      <img
-        src={idol.img}
-        alt={idol.name}
-        className="max-h-[600px] w-full rounded-t-xl object-cover"
-      />
+    return (
+        <div className="mt-8 rounded-xl bg-white">
+            <img
+                src={idol?.img}
+                alt={idol?.name}
+                className="max-h-[600px] w-full rounded-t-xl object-cover"
+            />
 
-      <p className="p-4 pt-8 text-lg text-gray-800">{idol.description}</p>
+            <p className="p-4 pt-8 text-lg text-gray-800">{idol?.description}</p>
 
-      <div className="flex items-center justify-between border-t border-gray-200 p-4">
-        <div className="flex items-center gap-6">
-          <button
-            type="button"
-            className="flex items-center gap-2 text-gray-600"
-          >
-            <Heart />
-            <span>144</span>
-          </button>
-          <button
-            type="button"
-            className="flex items-center gap-2 text-gray-600"
-          >
-            <Comment />
-            <span>144</span>
-          </button>
-        </div>
+            <div className="flex items-center justify-between border-t border-gray-200 p-4">
+                <div className="flex items-center gap-6">
+                    <button
+                        type="button"
+                        className="flex items-center gap-2 text-gray-600"
+                    >
+                        <Heart/>
+                        <span>144</span>
+                    </button>
+                    <button
+                        type="button"
+                        className="flex items-center gap-2 text-gray-600"
+                    >
+                        <Comment/>
+                        <span>144</span>
+                    </button>
+                </div>
 
-        <div className="relative" ref={ref}>
-          <button
-            type="button"
-            onClick={handleToggleIdolDropdown}
-            className="p-1 text-gray-400 hover:text-gray-600"
-          >
-            <Frame />
-          </button>
-          <Dropdown
-            isDropdownOpen={isIdolDropdownOpen}
-            handleToggleIdolDropdown={handleToggleIdolDropdown}
-            mode="comment"
-          >
-            <div className="px-4 py-2 text-center hover:bg-red-50">
-              <Link to={`/timeline/edit/${idol.id}`}>수정</Link>
+                <div className="relative" ref={ref}>
+                    <button
+                        type="button"
+                        onClick={handleToggleIdolDropdown}
+                        className="p-1 text-gray-400 hover:text-gray-600"
+                    >
+                        <Frame/>
+                    </button>
+                    <Dropdown
+                        isDropdownOpen={isIdolDropdownOpen}
+                        handleToggleIdolDropdown={handleToggleIdolDropdown}
+                        mode="comment"
+                    >
+                        <div className="px-4 py-2 text-center hover:bg-red-50">
+                            <Link to={`/timeline/edit/${idol?.id}`}>수정</Link>
+                        </div>
+                        <div>
+                            <button
+                                type="button"
+                                className="block w-full px-4 py-2 text-center text-sm text-red-500 hover:bg-red-50"
+                                onClick={handleDeletePost}
+                            >
+                                삭제
+                            </button>
+                        </div>
+                    </Dropdown>
+                </div>
             </div>
-            <div>
-              <button
-                type="button"
-                className="block w-full px-4 py-2 text-center text-sm text-red-500 hover:bg-red-50"
-                onClick={handleDeletePost}
-              >
-                삭제
-              </button>
-            </div>
-          </Dropdown>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default TimelineDetailCard;
