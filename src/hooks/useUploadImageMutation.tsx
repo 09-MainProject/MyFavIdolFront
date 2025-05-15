a6f931855292098e6f4cd5f74fc44449dd6094fa
Leekwanyong <dlrkdid12@gmail.com>
2025-05-15 22:36:52 +0900

    :sparkles: feat: #88 usePostImageManager hook 이미지 서버 api 훅 구현

--- src/hooks/useUploadImageMutation.tsx
+++ src/hooks/useUploadImageMutation.tsx
@@ -4,7 +4,9 @@ import {uploadImageApi, UploadImageRequest} from '@api/img/images.ts';
 function useUploadImageMutation() {
     return useMutation<UploadImageRequest, Error, UploadImageRequest>({
         mutationFn: (data) => uploadImageApi(data),
+        // eslint-disable-next-line no-console
         onSuccess: (result) => console.log('업로드 성공', result),
+        // eslint-disable-next-line no-console
         onError: () => console.error('업로드 실패')
     });
 }
