afc9cf9e8643fd326a294037f932b2fe125d30de
Leekwanyong <dlrkdid12@gmail.com>
2025-05-16 00:39:49 +0900

    :sparkles: feat: #88 성공, 실패 시 토스트 ui 구현

--- src/hooks/useUploadImageMutation.tsx
+++ src/hooks/useUploadImageMutation.tsx
@@ -1,13 +1,16 @@
 import {useMutation} from '@tanstack/react-query';
-import {uploadImageApi, UploadImageRequest} from '@api/img/images.ts';
+import {uploadImageApi, UploadImageRequest} from '@api/img/images';
+import PerformToast from '@utils/PerformToast';
 
 function useUploadImageMutation() {
     return useMutation<UploadImageRequest, Error, UploadImageRequest>({
         mutationFn: (data) => uploadImageApi(data),
-        // eslint-disable-next-line no-console
-        onSuccess: (result) => console.log('업로드 성공', result),
-        // eslint-disable-next-line no-console
-        onError: () => console.error('업로드 실패')
+        onSuccess: () => {
+            PerformToast({msg: '이미지 업로드 성공', type: 'success'});
+        },
+        onError: () => {
+            PerformToast({msg: '이미지 업로드 실패', type: 'error'});
+        }
     });
 }
 
