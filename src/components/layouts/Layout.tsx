8abbee6395b40f88e5b39dd16b89d3e1799f920e
Leekwanyong <dlrkdid12@gmail.com>
2025-05-16 00:40:06 +0900

    :sparkles: feat: #88 토스트컨테이너  연결

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -1,45 +1,48 @@
 import axios from 'axios';
-import { useEffect } from 'react';
-import { Outlet } from 'react-router';
-import { useAuthStore } from '@store/authStore.ts';
+import {useEffect} from 'react';
+import {Outlet} from 'react-router';
+import {ToastContainer} from 'react-toastify';
+import {useAuthStore} from '@store/authStore.ts';
 import Footer from './Footer';
 import Header from './Header/Header';
 import Breadcrumb from '../common/Breadcrumb';
 import ScrollTop from '../common/ScrollTop';
 
 function Layout() {
-  const { setLogin, csrfToken, accessToken, setUser } = useAuthStore();
+    const {setLogin, csrfToken, accessToken, setUser} = useAuthStore();
 
-  useEffect(() => {
-    if (!csrfToken || !accessToken) return;
-    setLogin(accessToken, csrfToken);
-    const fetchProfile = async () => {
-      try {
-        const response = await axios.get('/profile', {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        });
-        setUser(response.data.data);
-      } catch (e) {
-        // eslint-disable-next-line no-console
-        console.error(e);
-      }
-    };
-    fetchProfile();
-  }, [accessToken, csrfToken, setLogin, setUser]);
+    useEffect(() => {
+        if (!csrfToken || !accessToken) return;
+        setLogin(accessToken, csrfToken);
+        const fetchProfile = async () => {
+            try {
+                const response = await axios.get('/profile', {
+                    headers: {
+                        Authorization: `Bearer ${accessToken}`,
+                    },
+                });
+                setUser(response.data.data);
+            } catch (e) {
+                // eslint-disable-next-line no-console
+                console.error(e);
+            }
+        };
+        fetchProfile();
+    }, [accessToken, csrfToken, setLogin, setUser]);
 
-  return (
-    <div>
-      <Header />
-      <main className="py-8">
-        <Breadcrumb />
-        <Outlet />
-      </main>
-      <ScrollTop />
-      <Footer />
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <main className="py-8">
+                <Breadcrumb/>
+                <Outlet/>
+            </main>
+            <ToastContainer position="top-center" limit={1} closeButton={false} autoClose={3000} hideProgressBar/>
+
+            <ScrollTop/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default Layout;
