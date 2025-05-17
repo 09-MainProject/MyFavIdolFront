92f002fb0ea439617ec2653633b6b4232029dea7
Leekwanyong <dlrkdid12@gmail.com>
2025-05-18 01:17:39 +0900

    :bug: fix: #94 ci/cd 오류 수정

--- src/components/common/Profile/ProfileHeader.tsx
+++ src/components/common/Profile/ProfileHeader.tsx
@@ -1,21 +1,25 @@
+import useImageFallback from '@hooks/useImageFallback.tsx';
+import {UserProfileResponse} from '@/types/auth.ts';
+
 type Props = {
-  avatar: string;
-  nickname: string;
-  startDate?: string;
-  mode: 'post' | 'edit' | 'none';
+    mode: 'post' | 'edit' | 'none';
 };
 
-function ProfileHeader({ avatar, nickname, startDate, mode = 'post' }: Props) {
-  return (
-    <div className="mb-2 flex items-center gap-4">
-      <img src={avatar} alt={nickname} className="size-[50px] rounded-full" />
-      <div>
-        <p>{nickname}</p>
-        {mode === 'post' && <p>{startDate ?? '오류'}</p>}
-        {mode === 'edit' && <p>프로필 수정하기</p>}
-      </div>
-    </div>
-  );
+type User = Props & UserProfileResponse;
+
+function ProfileHeader({image_url, nickname, created_at, mode = 'post'}: User) {
+    const imgError = useImageFallback();
+    const formattedDate = created_at?.substring(0, 10);
+    return (
+        <div className="mb-2 flex items-center gap-4">
+            <img src={image_url} alt={nickname} className="size-[50px] rounded-full" onError={imgError}/>
+            <div>
+                <p>{nickname}</p>
+                {mode === 'post' && <p>{formattedDate}</p>}
+                {mode === 'edit' && <p>프로필 수정하기</p>}
+            </div>
+        </div>
+    );
 }
 
 export default ProfileHeader;
