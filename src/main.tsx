30af0832b89ff0e3156cc2a719abc53dc8392fc4
Leekwanyong <dlrkdid12@gmail.com>
2025-05-18 00:48:52 +0900

    :lipstick: style: #94 msw 실행 취소

--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,14 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-async function enableMocking() {
-  if (process.env.NODE_ENV !== 'development') {
-    return;
-  }
-
-  const { worker } = await import('./mocks/browser');
-  await worker.start({
-    onUnhandledRequest: 'bypass',
-    serviceWorker: {
-      url: '/mockServiceWorker.js',
-    },
-  });
-}
-
 const queryClient = new QueryClient();
-enableMocking().then(() => {
-  ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
+        <QueryClientProvider client={queryClient}>
+            <App/>
+        </QueryClientProvider>
     </React.StrictMode>
-  );
-});
+);
