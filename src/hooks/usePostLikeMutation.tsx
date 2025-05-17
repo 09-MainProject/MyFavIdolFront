ed8c20a6e167f4dc19c84e60125430381ef75fa0
Leekwanyong <dlrkdid12@gmail.com>
2025-05-18 01:11:31 +0900

    :sparkles: feat: #94 like mutation 좋아요 생성, 취소 구현

---
+++ src/hooks/usePostLikeMutation.tsx
@@ -0,0 +1,29 @@
+import {useMutation} from '@tanstack/react-query';
+import {createLikeApi, deleteLikeApi} from '@api/timeline/getLikes.ts';
+import PerformToast from '@utils/PerformToast.tsx';
+import {LikePostCreateRequest, LikePostCreateResponse, LikePostDeleteRequest} from '@/types/like.ts';
+
+function usePostLikeMutation() {
+    const createLikeMutation = useMutation<LikePostCreateResponse, Error, LikePostCreateRequest>({
+        mutationFn: (data) => createLikeApi(data),
+        onSuccess: () => {
+            PerformToast({msg: '좋아요 성공', type: 'success'});
+        },
+        onError: () => {
+            PerformToast({msg: '좋아요 실패', type: 'error'});
+        },
+    });
+
+    const deleteLikeMutation = useMutation<void, Error, LikePostDeleteRequest>({
+        mutationFn: (data) => deleteLikeApi(data),
+        onSuccess: () => {
+            PerformToast({msg: '좋아요 취소 성공', type: 'success'});
+        },
+        onError: () => {
+            PerformToast({msg: '좋아요 취소 실패', type: 'error'});
+        }
+    });
+    return {createLikeMutation, deleteLikeMutation};
+}
+
+export default usePostLikeMutation;
