c4b754b4bd408d15689f2d5a12b8b7727f1ae244
kjy0124 <0124kimjy@naver.com>
2025-05-17 17:24:33 +0900

    🐛 fix: #101 type 오류(코드 수정)

--- src/store/idolStore.ts
+++ src/store/idolStore.ts
@@ -3,16 +3,16 @@ import { create } from 'zustand/react';
 // Idol 삭제 필요 : 다른 페이지에서 사용하고 있는 부분이 있어 코드 삭제 후 수정 부탁드려요
 export interface Idol {
   id: number;
-  // idolId: number;
+  idolId: number;
   title: string;
   img: string;
-  // type: string;
+  type: string;
   startDate: string;
-  // endDate: string;
-  // location: string;
-  // description: string;
+  endDate: string;
+  location: string;
+  description: string;
   name: string;
-  // enName: string;
+  enName: string;
 };
 
 interface IdolState {
