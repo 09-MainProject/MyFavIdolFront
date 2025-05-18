d0d44bb6313f6017ccd22c61db6d2fefdd0a21e4
kjy0124 <0124kimjy@naver.com>
2025-05-19 03:16:07 +0900

    ♻️ refactor: #118 api 요청 로직을 idolApi 파일로 통합 및 axios 호출 제거

---
+++ src/api/idolApi.ts
@@ -0,0 +1,45 @@
+import { api } from '@/lib/api';
+
+// 아이돌 전체 목록 조회 
+export async function getIdolList(){
+  const res = await api.get('/idols');
+  return res.data;
+}
+
+// 팔로우 목록 조회
+export async function getFollowedIdolList(){
+    const res = await api.get('/idols/follows');
+    return res.data;
+}
+
+// 팔로우 삭제
+export async function unfollowIdol(id:number){
+    await api.delete(
+        `/idols/${id}/follows`
+      );
+}
+
+// 팔로우 상태 확인
+export async function checkFollowState(id:number){
+    const res = await api.get(
+      `/idols/${id}/follow-status`
+    );
+    return res.data.data.is_following;
+}
+
+// 팔로우 
+export async function followIdol(id:number){
+    await api.post(
+        `/idols/${id}/follows`
+      );
+}
+
+// 관리자기능 - 아이돌 삭제 
+export async function deleteIdol(id:number){
+    await api.delete(`/idols${id}`);
+}
+// 아이돌 상세 정보 
+export async function getIdolDetailData(id:number){
+    const res = await api.get(`/idols${id}`);
+    return res.data;
+}
