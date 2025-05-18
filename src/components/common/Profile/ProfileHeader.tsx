7fbf98af2fc91e4141ac557aa40565953cae8337
Leekwanyong <dlrkdid12@gmail.com>
2025-05-18 23:16:22 +0900

    :bug: fix: #94 ci/cd 오류 수정

--- src/components/common/Profile/ProfileHeader.tsx
+++ src/components/common/Profile/ProfileHeader.tsx
@@ -1,4 +1,5 @@
 import useImageFallback from '@hooks/useImageFallback.tsx';
+import useImgLazy from '@hooks/useImgLazy.tsx';
 import {UserProfileResponse} from '@/types/auth.ts';
 
 type Props = {
@@ -9,13 +10,18 @@ type User = Props & UserProfileResponse;
 
 function ProfileHeader({image_url, nickname, created_at, mode = 'post'}: User) {
     const imgError = useImageFallback();
+    const imgRef = useImgLazy();
     const formattedDate = created_at?.substring(0, 10);
     return (
-        <div className="mb-2 flex items-center gap-4">
-            <img src={image_url} alt={nickname} className="size-[50px] rounded-full" onError={imgError}/>
+        <div className="pb-3 flex items-center gap-4">
+            <picture>
+                <source srcSet={image_url} type="image/webp"/>
+                <img ref={imgRef} src={image_url} alt={nickname} className="size-[42px] rounded-full"
+                     onError={imgError}/>
+            </picture>
             <div>
-                <p>{nickname}</p>
-                {mode === 'post' && <p>{formattedDate}</p>}
+                <p className="text-sm">{nickname}</p>
+                {mode === 'post' && <p className="text-sm">{formattedDate}</p>}
                 {mode === 'edit' && <p>프로필 수정하기</p>}
             </div>
         </div>
--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -1,58 +1,54 @@
-import { create } from 'zustand';
-import { createJSONStorage, persist } from 'zustand/middleware';
+import {create} from 'zustand';
+import {createJSONStorage, persist} from 'zustand/middleware';
 
 type User = {
-  nickname: string;
-  profileImage: string;
-  commentAlarm: boolean;
-  likeAlarm: boolean;
-  scheduleAlarm: boolean;
-  is_staff: boolean;
-  is_superuser: boolean;
+    nickname: string;
+    profileImage: string;
+    commentAlarm: boolean;
+    likeAlarm: boolean;
+    scheduleAlarm: boolean;
+    is_staff: boolean;
+    is_superuser: boolean;
 };
 
 interface AuthState {
-  user: User | null;
-  login: boolean;
-  accessToken: string | null;
-  csrfToken: string | null;
+    user: User | null;
+    login: boolean;
+    accessToken: string | null;
+    csrfToken: string | null;
 }
 
 interface AuthActions {
-  setLogin: (accessToken: string, csrfToken: string) => void;
-  setLogout: () => void;
-  isAuthenticated: () => boolean;
-  setUser: (user: User) => void;
+    setLogin: (accessToken: string, csrfToken: string) => void;
+    setLogout: () => void;
+    isAuthenticated: () => boolean;
+    setUser: (user: User) => void;
 }
 
 type AuthStore = AuthState & AuthActions;
 
-export const useAuthStore = create<AuthStore>()(
-  persist(
-    (set, get) => ({
-      user: null,
-      login: false,
-      accessToken: null,
-      csrfToken: null,
-      setLogin: (accessToken: string, csrfToken: string) => {
-        if (!accessToken || !csrfToken) {
-          throw new Error('Invalid tokens provided');
+export const useAuthStore = create<AuthStore | Partial<AuthStore>>()(
+    persist(
+        (set) => ({
+            user: null,
+            login: false,
+            accessToken: null,
+            csrfToken: null,
+            setLogin: (accessToken: string, csrfToken: string) => {
+                if (!accessToken || !csrfToken) {
+                    throw new Error('Invalid tokens provided');
+                }
+                set({accessToken, csrfToken, login: true});
+            },
+            setUser: (user: User) =>
+                set(state => ({user: {...state.user, ...user}})),
+            setLogout: () => {
+                set({accessToken: null, csrfToken: null, login: false, user: null});
+            },
+        }),
+        {
+            name: 'login-token',
+            storage: createJSONStorage(() => localStorage),
         }
-        set({ accessToken, csrfToken, login: true });
-      },
-      setUser: (user: User) =>
-        set(state => ({ user: { ...state.user, ...user } })),
-      setLogout: () => {
-        set({ accessToken: null, csrfToken: null, login: false, user: null });
-      },
-      isAuthenticated: () => {
-        const { accessToken, csrfToken } = get();
-        return Boolean(accessToken && csrfToken);
-      },
-    }),
-    {
-      name: 'login-token',
-      storage: createJSONStorage(() => localStorage),
-    }
-  )
+    )
 );
