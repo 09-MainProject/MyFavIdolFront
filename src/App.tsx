e988ecdf6461be32768966b569e6a11c1af15aa0
kjy0124 <0124kimjy@naver.com>
2025-05-21 04:05:48 +0900

    🐛 fix: #122 ci/cd 오류 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import { createBrowserRouter, RouterProvider } from 'react-router';
-
+import {createBrowserRouter, RouterProvider} from 'react-router';
+import ScheduleCreatePage from '@pages/schedule/ScheduleCreatePage.tsx';
+import TimelineForm from '@pages/timeline/TimelineForm.tsx';
 import Layout from '@/components/layouts/Layout';
 
 import Artist from '@/pages/artists/Artist';
-import ArtistDetail from './pages/artists/ArtistDetail';
-import CreateArtist from './pages/artists/CreateArtist';
-import EditArtist from './pages/artists/EditArtist';
 import Home from '@/pages/home/Home';
 import Schedule from '@/pages/schedule/Schedule';
-import ScheduleCreatePage from '@pages/schedule/ScheduleCreatePage.tsx';
 import ScheduleDetail from '@/pages/schedule/ScheduleDetail';
 import CheckPassword from '@/pages/signup/CheckPassword';
 import EditProfile from '@/pages/signup/EditProfile';
@@ -17,7 +14,9 @@ import Profile from '@/pages/signup/Profile';
 import Signup from '@/pages/signup/Signup';
 import Timeline from '@/pages/timeline/Timeline';
 import TimelineDetail from '@/pages/timeline/TimelineDetail';
-import TimelineForm from '@pages/timeline/TimelineForm.tsx';
+import ArtistDetail from './pages/artists/ArtistDetail';
+import CreateArtist from './pages/artists/CreateArtist';
+import EditArtist from './pages/artists/EditArtist';
 
 
 function App() {
