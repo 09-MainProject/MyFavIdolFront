f2a25b00e311ffde4d8641108be836126f415c37
Leekwanyong <dlrkdid12@gmail.com>
2025-05-20 16:08:27 +0900

    :sparkles: feat: #117 댓글 type 정의

--- src/types/comment.ts
+++ src/types/comment.ts
@@ -1,28 +1,35 @@
 export type CommentListItem = {
-  id: number;
-  post: number;
-  author: { nickname: string; name: string };
-  content: string;
-  created_at: string;
-  updated_at: string;
-  is_deleted: boolean;
-  likes_count: number;
-  is_liked: string;
+    id: number;
+    post: number;
+    author: string;
+    content: string;
+    created_at: string;
+    updated_at: string;
+    is_deleted: boolean;
+    likes_count: number;
+    parent: number | null;
+    is_liked: string;
 };
 
 export type CommentListResponse = {
-  count: number;
-  next: string;
-  previous: string;
-  results: CommentListItem[];
+
+    count: number;
+    next: string;
+    previous: string;
+    results: CommentListItem[];
 };
 
 export type CommentCreateRequest = {
-  content: string;
-  parentId: number;
-  id?: number;
+    content: string;
+    parent: number | null;
+    id?: number;
 };
 
+export type CommentUpdateRequest = {
+    content: string;
+    parent: number | null;
+}
+
 export type CommentResponse = {
-  content: string;
+    content: string;
 };
