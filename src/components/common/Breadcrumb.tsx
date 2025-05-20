00bae6f58cc09cfebad180e2cb6535f853f59811
kjy0124 <0124kimjy@naver.com>
2025-05-20 14:02:58 +0900

    :sparkles: feat: #122 Breadcrumb 경로에 아이돌 ID 대신 이름 표시 기능 추가

--- src/components/common/Breadcrumb.tsx
+++ src/components/common/Breadcrumb.tsx
@@ -1,6 +1,8 @@
 import { Link, useLocation } from 'react-router';
+import useIdolData from '@/hooks/useIdolData';
 
 function Breadcrumb() {
+  const { idolList } = useIdolData();
   const location = useLocation();
   const paths = location.pathname.split('/').filter(Boolean);
   return (
@@ -8,15 +10,19 @@ function Breadcrumb() {
       {location.pathname === '/' ? null : (
         <div>
           <Link to="/">Home</Link>
-          {paths.map((segment, idx) => (
-            // schedule로 예를 들면 slice 메서드를 사용하여 0번째 인덱스 즉 schedule이 되고 idx+1는 아이돌 아이디를 가리킴
-            <Link to={`/${paths.slice(0, idx + 1).join('/')}`}>
-              <span key={location.key}>
-                {' '}
-                {'>'} {segment}
-              </span>
-            </Link>
-          ))}
+          {
+            paths.map((segment, idx) => {
+              const matchedIdol = idolList.find((idol)=> String(idol.id) === segment);
+              const label = matchedIdol ? matchedIdol.name : segment;
+              const path = `/${paths.slice(0, idx + 1).join('/')}`;
+              
+              return (
+                <Link to={path} key={path}>
+                  <span>{'>'} {label}</span>
+                </Link>
+              );
+            })
+          }
         </div>
       )}
     </div>
