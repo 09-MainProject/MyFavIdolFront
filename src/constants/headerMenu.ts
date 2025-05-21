ad0e553c79042eabfa659fa2b0177e761c40b6f8
Leekwanyong <dlrkdid12@gmail.com>
2025-05-21 09:09:10 +0900

    :lipstick: style: #129 header_menu 타이틀 영어로 수정 index.css button 전역으로 포인터 추가

--- src/constants/headerMenu.ts
+++ src/constants/headerMenu.ts
@@ -1,10 +1,10 @@
 export interface HeaderMenuItem {
-  title: string;
-  href: string;
+    title: string;
+    href: string;
 }
 
 export const HEADER_MENU: HeaderMenuItem[] = [
-  { title: '아티스트', href: '/artists' },
-  { title: '스케줄', href: '/schedule' },
-  { title: '타임라인', href: '/timeline' },
+    {title: 'Artists', href: '/artists'},
+    {title: 'Schedule', href: '/schedule'},
+    {title: 'Timeline', href: '/timeline'},
 ];
